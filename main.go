66a21cba227842ce6d879111eba95ca1ef3d459d
Giovanni Harting <539@idlegandalf.com>
2021-11-20 00:06:44 +0100

    ignore dbpackage not existing in repoPurge

--- main.go
+++ main.go
@@ -458,7 +458,7 @@ func (b *BuildManager) repoWorker(repo string) {
 			}
 
 			if pkg.DbPackage != nil {
-				pkg.DbPackage = pkg.DbPackage.Update().ClearRepoVersion().SaveX(context.Background())
+				_ = pkg.DbPackage.Update().ClearRepoVersion().Exec(context.Background())
 			}
 
 			for _, file := range pkg.PkgFiles {
